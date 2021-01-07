6f9c43fcf1de9d92e12640e4a13424a9c6bb9518
manggic <manishmahto198@gmail.com>
2021-01-07 16:04:58 +0530

    first commit

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,15 @@
+export default {
+  //TODO:
+  // add your keys here
+  
+  // var firebaseConfig = {
+    apiKey: "AIzaSyAQuWW-EznTXKQosVQTVEFbUufrEUOZMpQ",
+    authDomain: "mygitapp-3f038.firebaseapp.com",
+    databaseURL: "https://mygitapp-3f038.firebaseio.com",
+    projectId: "mygitapp-3f038",
+    storageBucket: "mygitapp-3f038.appspot.com",
+    messagingSenderId: "965741536577",
+    appId: "1:965741536577:web:4c8ea10a278980b5f16dc0",
+    measurementId: "G-WLLZNC6LTG"
+  // };
+};
---
+++ src/pages/Home.js
@@ -0,0 +1,69 @@
+import React, { useState, useContext } from "react";
+import Axios from "axios";
+
+import {
+  Row,
+  Container,
+  Col,
+  Input,
+  Button,
+  InputGroup,
+  InputGroupAddon
+} from "reactstrap";
+
+import UserCard from "../components/UserCard";
+import Repos from "../components/Repos";
+import { Redirect } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+import { toast } from "react-toastify";
+
+const Home = () => {
+  const context = useContext(UserContext);
+  const [query, setQuery] = useState("hiteshChoudhary");
+  const [user, setUser] = useState(null);
+
+
+  // fetch the details by user name
+  // store the data in user state
+  const fetchDetails = async () => {
+    try {
+      const { data } = await Axios.get(`https://api.github.com/users/${query}`);
+      setUser(data);
+      console.log({ data });
+    } catch (eror) {
+      toast("Not able to locate user", { type: "error" });
+    }
+  };
+
+  
+  // if context.user is empty then move to signIn page
+  if (!context.user?.uid) {
+    return <Redirect to="/signin" />;
+  }
+
+  return (
+    <Container>
+      <Row className=" mt-3">
+        <Col md="5">
+          <InputGroup>
+            <Input
+              type="text"
+              value={query}
+              onChange={e => setQuery(e.target.value)}
+              placeholder="Please provide the username"
+            />
+            <InputGroupAddon addonType="append">
+              <Button onClick={fetchDetails} color="primary">
+                Fetch User
+              </Button>
+            </InputGroupAddon>
+          </InputGroup>
+          {user ? <UserCard user={user} /> : null}
+        </Col>
+        <Col md="7">{user ? <Repos repos_url={user.repos_url} /> : null}</Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Home;
